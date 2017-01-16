e250c667129d83ea988dd80a6113752fcad83124
cyrilluce <cyrilluce@gmail.com>
2017-01-17 03:51:44 +0800

    多数据库版请求缓冲

--- src/stores/query/item.ts
+++ src/stores/query/item.ts
@@ -7,45 +7,44 @@ import { Query, Result } from '../../routers/query/item';
  * 添加缓存机制？
  * 以及合并查询机制
  */
-
-
-
-let queue = [];
-let waiting, fetching;
+const queueMap = {};
+const waitingMap = {};
+const fetchingMap = {};
 export async function item(param: Query) {
     let ids: number[] = [].concat(param.id);
     const loc = param.loc;
     let delayTask = delay(global.IS_BROWSER ? 100 : 0);
+    let queue = queueMap[loc] || (queueMap[loc] = []);
 
     queue.push(...ids);
 
     // 如果正在请求，等上次的完成
-    if (fetching) {
-        await fetching;
+    if (fetchingMap[loc]) {
+        await fetchingMap[loc];
     }
     // 如果是空闲状态了，进入等待
-    if (!waiting) {
-        waiting = delayTask;
-        await waiting;
-        waiting = null;
+    if (!waitingMap[loc]) {
+        waitingMap[loc] = delayTask;
+        await waitingMap[loc];
+        waitingMap[loc] = null;
     }else{ // 如果别人已经先排好队了，坐等开车
-        await waiting;
+        await waitingMap[loc];
     }
     
 
     let map;
     // 等待结束后，开始查询，其它人没查，就自己开车
-    if (!fetching) {
-        fetching = fetch<Result>('/node/query/item', {
+    if (!fetchingMap[loc]) {
+        fetchingMap[loc] = fetch<Result>('/node/query/item', {
             loc,
-            id: queue
+            id: queueMap[loc]
         });
-        queue = [];
-        map = await fetching;
-        fetching = null;
+        queueMap[loc] = [];
+        map = await fetchingMap[loc];
+        fetchingMap[loc] = null;
     }else{
         // 如果别人已经开始查询了，坐等快递
-        map = await fetching;
+        map = await fetchingMap[loc];
     }
 
     return map;
